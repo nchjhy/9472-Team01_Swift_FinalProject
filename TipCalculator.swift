3bfcce3450e846f7795cf44feb88977d7d5384fb
Chloe <chloeleecsm710@gmail.com>
2024-11-19 16:08:28 +0800

    Update TipCalculator.swift
    Added declarations and struct ContentView_Previews

--- TipCalculator.swift
+++ TipCalculator.swift
@@ -1,1 +1,15 @@
+import SwiftUI
+
+struct TipCalculator: View {
+    @State private var enteredAmount: String = ""
+    @State private var tipAmount: Double = 0
+    @State private var totalAmount: Double = 0
+    @State private var tipSlider: Double = 15
+  
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            TipCalculator()
+        }
+    }
+}
 
